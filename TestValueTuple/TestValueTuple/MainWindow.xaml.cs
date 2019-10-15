84c3f9c9fef146f3854f3151d62f98e0c5d8403a
ziizai <312009888@qq.com>
2019-10-15 16:06:58 +0800

    a

--- TestValueTuple/TestValueTuple/MainWindow.xaml.cs
+++ TestValueTuple/TestValueTuple/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management.Automation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -34,6 +35,8 @@ namespace TestValueTuple
 
         public int Foo()
         {
+            AliasInfo
+
             return 1;
         }
 
